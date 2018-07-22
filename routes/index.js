533bb26153f2d8dcfa94a8f9be771dd3281906d2
Jeff Wood <jeffwood1992@gmail.com>
2018-07-22 08:13:52 +0000

    responsive nav bar

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get("/", function(req, res){
 
 //show the register form
 router.get("/register", function(req, res){
-    res.render("register"); 
+    res.render("register", {page: "register"}); 
 });
 
 //handle sign up logic
@@ -35,7 +35,7 @@ router.post("/register", function(req, res){
 
 //show the login form
 router.get("/login", function(req, res){
-   res.render("login") ;
+   res.render("login", {page: "login"});
 });
 
 //handling login logic
